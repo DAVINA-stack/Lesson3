f34bd90254a147089dfbd652ad9b44a0393a0136
DAVINA <davinabuansi@gmail.com>
2024-11-29 12:42:36 +0000

    push all changest

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+let x =prompt("give me a number ");
+let booking = prompt("let is know if you like to cancel, comfirm or reschedule");
+alert
+
+if (booking =" confirm")(
+ alert("see you soon");   
+)else if (booking == "cancel")(
+    alert("appointment cancelled");
+)else if(booking =="reschedule")(
+    alert("reschedule");
+)
